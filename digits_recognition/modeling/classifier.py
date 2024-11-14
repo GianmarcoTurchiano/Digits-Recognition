2a2f8c31a7f37df4109fb3d743a21451ea82a7d6
Gianmarco Turchiano <gianmarco.turchiano@outlook.it>
2024-11-14 21:45:06 +0100

    Implemented architecture with 2 hidden layers.

--- digits_recognition/modeling/classifier.py
+++ digits_recognition/modeling/classifier.py
@@ -6,10 +6,12 @@ class DigitClassifier(nn.Module):
         super(DigitClassifier, self).__init__()
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(28 * 28, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 10)
 
     def forward(self, x):
         x = self.flatten(x)
         x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
